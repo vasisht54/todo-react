8077482ebd05fca2ee1d4be7872273c1c253ea95
vasisht54 <vasisht.theviper@gmail.com>
2020-09-20 12:42:38 -0400

    Added a todo viewer ui component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Todos from "./ui-components/Todos"
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {id: 1, content: "Buy parathas"},
+      {id: 2, content: "Clean up the kitchen counter"},
+      {id: 3, content: "Watch 'The Office'"}
+    ]
+  };
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">To-do List</h1>
+        <Todos todos = {this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
