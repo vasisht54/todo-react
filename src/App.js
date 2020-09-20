5c68860dd36bb4fa76004597c0e025395912f6b8
vasisht54 <vasisht.theviper@gmail.com>
2020-09-20 12:54:46 -0400

    Added delete todo feature

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ class App extends React.Component {
     ]
   };
 
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos
+    })
+  }
+
   render() {
     return (
       <div className="todo-app container">
-        <h1 className="center blue-text">To-do List</h1>
-        <Todos todos = {this.state.todos} />
+        <h1 className="center red-text">To-do List</h1>
+        <Todos todos = {this.state.todos} deleteTodo = {this.deleteTodo}/>
       </div>
     );
   }
