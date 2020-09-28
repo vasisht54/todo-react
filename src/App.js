8d0ef9df9c32440b314969cc2437cf54c98bda7c
vasisht54 <vasisht.theviper@gmail.com>
2020-09-28 12:08:36 -0400

    Changed default mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import TodoList from './components/TodoList';
 
 function App() {
@@ -8,49 +8,6 @@ function App() {
       <TodoList />
     </div>
   );
-
-  // state = {
-  //   todos: [
-  //     {id: uuid(), content: "Buy parathas"},
-  //     {id: uuid(), content: "Clean up the kitchen counter"},
-  //     {id: uuid(), content: "Watch 'The Office'"}
-  //   ]
-  // };
-
-  // deleteTodo = (id) => {
-  //   const todos = this.state.todos.filter(todo => {
-  //     return todo.id !== id;
-  //   });
-  //   this.setState({
-  //     todos
-  //   })
-  // }
-
-  // addTodo = (todo) => {
-  //   todo.id = uuid();
-  //   let todos = [...this.state.todos, todo];
-  //   this.setState({
-  //     todos
-  //   })
-  // }
-
-  // render() {
-
-    // return (
-    //   {/* <div className="todo-app container">
-    //     <ThemeContextProvider>
-    //       <h1 className="center red-text">To-do List</h1>
-    //       <div className="center">Click on an item to delete it</div>
-    //       <Todos todos = {this.state.todos} deleteTodo = {this.deleteTodo}/>
-    //       <AddTodo addTodo = {this.addTodo} />
-    //       <ThemeToggler/>
-    //     </ThemeContextProvider>
-    //   </div> */}
-    //  <div classN>
-
-    //  </div>
-    // );
- // }
 }
 
 export default App;
